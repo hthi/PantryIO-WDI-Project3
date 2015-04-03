15c1fe47a00d568ac5a688083c1dbbcf4f24cd38
Helen Thi <ht5658@gmail.com>
2015-04-03 15:16:27 -0400

    added bcrypt gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails-api'
 
 gem 'active_model_serializers'
 
+gem 'bcrypt'
+
 gem 'rack-cors', :require => 'rack/cors'
 
 gem 'spring', :group => :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+RecipeCollection.delete_all
+User.delete_all
+Recipe.delete_all
+Ingredient.delete_all
+
+user.create!()
+
+user.recipes.create
+
+ingredient.create
+
+RecipeCollection.create
